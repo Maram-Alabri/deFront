aad91225a37d931aada55f432f5bae8576df8f4c
maram <maramalabri22@gmail.com>
2025-07-28 15:42:04 +0400

    return without x

---
+++ withoutX.java
@@ -0,0 +1,17 @@
+public class withoutX {
+    public String withoutX(String str) {
+        int len = str.length();
+
+        if (len > 0 && str.charAt(0) == 'x') {
+            str = str.substring(1);
+            len--;
+        }
+
+        if (len > 0 && str.charAt(str.length() - 1) == 'x') {
+            str = str.substring(0, str.length() - 1);
+        }
+
+        return str;
+    }
+
+}
