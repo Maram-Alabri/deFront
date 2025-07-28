39705efee2757cb4533222a21dd6ee0f572b22cb
maram <maramalabri22@gmail.com>
2025-07-28 15:03:39 +0400

    solve ques

---
+++ nTwice.java
@@ -0,0 +1,9 @@
+public class nTwice {
+    public String nTwice(String str, int n) {
+        String firstPart = str.substring(0, n);
+        String lastPart = str.substring(str.length() - n);
+        return firstPart + lastPart;
+
+    }
+
+}
