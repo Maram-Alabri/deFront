d359089ff8321cf4ad1ff3c61adc1c40ab5322e4
maram <maramalabri22@gmail.com>
2025-07-28 14:39:54 +0400

    if it is match

--- seeColor.java
+++ seeColor.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class seeColor {
+    public String seeColor(String str) {
+        if (str.startsWith("red")) {
+            return "red";
+        } else if (str.startsWith("blue")) {
+            return "blue";
+        }
+        return "";
+    }
 }
